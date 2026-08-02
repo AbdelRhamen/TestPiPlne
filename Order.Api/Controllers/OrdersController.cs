83cd2c4609eef29229342895b999b0e7a353e467
Abdelrahmen <76070248+AbdelRhamen@users.noreply.github.com>
2026-08-03 00:08:16 +0300

    r

--- Order.Api/Controllers/OrdersController.cs
+++ Order.Api/Controllers/OrdersController.cs
@@ -54,15 +54,6 @@ namespace Order.Api.Controllers
             foreach (var o in orders)
             {
 
-
-
-
-
-
-
-
-
-
                
 
                 var user = await _userClient.GetUserDetailsAsync(o.CreatedBy);
